6362c0f1d2e7e6b05cdb55d9869de9b05b7b67d7
Mihai <mihai.banulescu@gmail.com>
2016-07-22 11:59:09 -0700

    add pic_url to picture_serializer

--- app/serializers/picture_serializer.rb
+++ app/serializers/picture_serializer.rb
@@ -1,3 +1,3 @@
 class PictureSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :location_id, :description, :title
+  attributes :id, :user_id, :location_id, :description, :title, :pic_url
 end
